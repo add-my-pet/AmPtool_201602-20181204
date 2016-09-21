923e66e3161292a21f71f7962428e8e3b3a1dc6e
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2016-09-21 16:29:46 +0200

    scripts for publishing the collection

--- run_species_html.m
+++ run_species_html.m
@@ -9,13 +9,12 @@ WD = pwd; % store current path
 % make species.html
 
 fid_Spec = open_species_html; % open up species.html for writing and delete the old file
-
 for i = 1:n
     fprintf('%g/ %g : %s \n',i,n, entries{i} ) 
     cd(['../entries/',entries{i}]) % goto entries 
     load(['results_',entries{i},'.mat']) % load results_my_pet.mat 
-    print_species_html(metaData, metaPar, fid_Spec)
     cd(WD)
+    print_species_html(metaData, metaPar, fid_Spec)
 end
 close_species_html(fid_Spec); % close species.html
 
