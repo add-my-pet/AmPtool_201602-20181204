cff04dd4c5500da2147c14baf44d998d640b61f6
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2016-11-03 10:20:40 +0100

    species list
    small correction

--- run_species_list_html.m
+++ run_species_list_html.m
@@ -15,14 +15,13 @@ n = length(entries);
 
 fid_Spec = open_species_list_html; % open up species_list.html for writing and delete the old file
 for i = 1:n
-  if strcmp(entries{i},'Crinia_nimbus')==0 && strcmp(entries{i},'Geocrinia_vitellina')==0
     fprintf('%g/ %g : %s \n',i,n, entries{i} ) 
     cd(['../entries/',entries{i}]) % goto entries 
     load(['results_',entries{i},'.mat']) % load results_my_pet.mat 
     cd(WD)
     prt_species_list_html(metaData, metaPar, fid_Spec)
-  end
 end
 close_species_list_html(fid_Spec); % close species_list.html
 
 copyfile('species_list.html','../.')
+delete('species_list.html') % delete the files that is in entries_admin
