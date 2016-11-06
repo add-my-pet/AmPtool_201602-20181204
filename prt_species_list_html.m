74a2062cdfd6df673012f2b8c4db5ad3fb97720a
baskooijman <bas.kooijman@vu.nl>
2016-11-06 07:22:02 +0100

    reorganisation and naming of list rows

--- prt_species_list_html.m
+++ prt_species_list_html.m
@@ -7,14 +7,16 @@ function prt_species_list_html
 %   modified 2015/07/21 Starrlight Augustine; 2015/08/28 Starrlight Augustine; 2016/11/05 Bas Kooijman
 
 %% Syntax
-% <../prt_species_list_html.m *prt_species_list_html*> (metaData, metaPar, fidSpec) 
+% <../prt_species_list_html.m *prt_species_list_html*>
 
 %% Description
 % deletes and writes ../species_list.html
 
 %% Remarks
-% uses open_species_list_html, prt_species_row and close_species_list_html
-% expects to find /entries
+% uses subfunctions open_species_list_html, prt_species_row and close_species_list_html
+% expects to find /entries with all entries given by DEBtool_M/taxa/select
+% each row in the table has a name
+% first writes species_list.html in entries_admin, then moves it to ../entries_admin
 
 WD = pwd; % store current path
 
@@ -26,8 +28,6 @@ allNames = {allFiles.name};
 
 cd(WD) % return to current path
 n = length(entries);
-
-% make species_list.html
 
 fid_Spec = open_species_list_html; % open up species_list.html for writing and delete the old file
 for i = 1:n
@@ -41,3 +41,144 @@ close_species_list_html(fid_Spec); % close species_list.html
 
 copyfile('species_list.html','../.')
 delete('species_list.html') % delete the file that is in entries_admin
+
+end
+
+%% subfunction open_species_list_html 
+% opens up species_list.html for reading and writing
+ 
+function fid_Spec = open_species_list_html
+% created by Bas Kooijman; modified 2015/04/14 Starrlight, 2016/11/03 Bas Kooijman
+
+% Syntax
+% fid_Spec = <../open_species_list_html.m *open_species_list_html*> 
+
+% Description
+%
+% Output:
+% 
+% * fid_Spec: scalar
+
+% Remarks
+% This deletes the existing species_list.html
+
+% Example of use
+% fid_Spec = open_species_list_html
+
+% if exist('n_spec','var')==0
+%   n_spec = 1;  % initiate species numbers
+
+fid_Spec = fopen('species_list.html', 'w+'); % open file for writing, delete existing content
+  
+% make the header for species_list.html :
+fprintf(fid_Spec, '<!DOCTYPE html>\n');
+fprintf(fid_Spec, '<HTML>\n');
+fprintf(fid_Spec, '<HEAD>\n');
+fprintf(fid_Spec, '  <TITLE>Species List</TITLE>\n');
+fprintf(fid_Spec, '  <link rel="stylesheet" type="text/css" href="sys/style.css"> \n');
+fprintf(fid_Spec, '  <script src="sys/dropdown.js"></script>\n');
+fprintf(fid_Spec, '  <script src="sys/w3data.js"></script>\n');
+fprintf(fid_Spec, '</HEAD>\n\n');
+fprintf(fid_Spec, '<BODY>\n\n');
+
+fprintf(fid_Spec, '<div w3-include-html="sys/wallpaper_amp.html"></div>\n');
+fprintf(fid_Spec, '<script>w3IncludeHTML();</script>\n\n');
+
+fprintf(fid_Spec, '<div w3-include-html="sys/toolbar_list.html"></div>\n');
+fprintf(fid_Spec, '<script>w3IncludeHTML();</script>\n\n');
+	
+fprintf(fid_Spec, '<div id = "main">\n');
+fprintf(fid_Spec, '  <div id = "main-wrapper-species">    \n');
+fprintf(fid_Spec, '    <div id="contentFull">\n');
+fprintf(fid_Spec, '      <TABLE>\n');
+fprintf(fid_Spec, '        <TR HEIGHT=60 BGCOLOR = "#FFE7C6">\n');
+fprintf(fid_Spec, '          <TH><a class="link" href="#" onclick="BoxArt_phylum();">phylum</a></TH>\n');
+fprintf(fid_Spec, '          <TH>class</TH> <TH>order</TH> <TH>family</TH> <TH>species</TH> <TH>common name</TH>\n');
+fprintf(fid_Spec, '          <TH BGCOLOR = "#FFC6A5"><a class="link" target = "_blank" href="http://www.debtheory.org/wiki/index.php?title=Typified_models">&nbsp; type &nbsp;</a></TH>\n');
+fprintf(fid_Spec, '          <TH BGCOLOR = "#FFE7C6"><a class="link" target = "_blank" href="http://www.debtheory.org/wiki/index.php?title=Add-my-pet_Introduction#Goodness_of_fit_criterion" >&nbsp; MRE &nbsp;</a></TH>\n');
+fprintf(fid_Spec, '          <TH BGCOLOR = "#FFCE9C"><a class="link" target = "_blank" href="http://www.debtheory.org/wiki/index.php?title=Completeness" >&nbsp; complete &nbsp;</a></TH>\n');
+fprintf(fid_Spec, '          <TH BGCOLOR = "#FFFFC6"><a class="link" target = "_blank" href="http://www.debtheory.org/wiki/index.php?title=Data_types" >&nbsp; data &nbsp;</a></TH>\n');
+fprintf(fid_Spec, '        </TR>\n\n');
+
+end
+
+%% subfunction prt_species_row
+% places a line in species_list.html which has previously been opened for reading and writing
+
+function prt_species_row(metaData, metaPar, fidSpec)
+% created by Bas Kooijman; modified 2015/04/14 Starrlight Augustine & Goncalo Marques; 
+%   modified 2015/07/21 Starrlight Augustine; 2015/08/28 Starrlight Augustine; 2016/11/05 Bas Kooijman
+
+% Syntax
+% <../prt_species_row.m *prt_species_row*> (metaData, metaPar, fidSpec) 
+
+% Description
+% Print the line in ../species.html for a pet
+%
+% Input:
+%
+% * metaData: structure 
+% * metaPar: structure
+% * fidSpec: scalar
+
+v2struct(metaData); v2struct(metaPar);
+
+% Remove underscores 
+% Puts first letter of english name in capital:
+speciesprintnm = strrep(metaData.species, '_', ' ');
+speciesprintnm_en = strrep(metaData.species_en, '_', ' ');
+if speciesprintnm_en(1)>='a' && speciesprintnm_en(1)<='z'
+  speciesprintnm_en(1)=char(speciesprintnm_en(1)-32);
+end
+
+n_data_0 = length(data_0); n_data_1 = length(data_1); 
+  
+
+  fprintf(fidSpec,['        <TR name = "', species, '">\n']);
+  fprintf(fidSpec,['          <TD>', phylum, '</TD>  <TD>', metaData.class, '</TD> <TD>', order, '</TD> <TD>', family, '</TD>\n']);
+  fprintf(fidSpec,['          <TD><A TARGET="_top" HREF="entries_web/results_', species, '.html">', speciesprintnm, '</A></TD> <TD>', speciesprintnm_en, '</TD>\n']);
+  fprintf(fidSpec, '          <TD style="text-align:center"  BGCOLOR = "#FFC6A5">%s</TD>\n', model);
+  fprintf(fidSpec, '          <TD style="text-align:center"  BGCOLOR = "#FFE7C6">%8.3f</TD>\n', MRE);
+  fprintf(fidSpec, '          <TD style="text-align:center"  BGCOLOR = "#FFCE9C">%g</TD>\n', COMPLETE);
+  for i = 1:n_data_0
+    fprintf(fidSpec, '          <TD BGCOLOR = "#FFFFC6">%s</TD>\n', data_0{i});      
+  end
+  for i = 1:n_data_1
+    fprintf(fidSpec, '          <TD BGCOLOR = "#FFFF9C">%s</TD>\n', data_1{i});  
+  end
+  fprintf(fidSpec, '        </TR>\n\n');
+  
+end
+
+%% subfunction close_species_html(fid_Spec)
+% closes species_list.html
+
+function close_species_list_html(fidSpec)
+% created by Bas Kooijman; modified 2015/08/28 Starrlight Augustine, 2016/11/03 Bas Kooijman
+
+% Syntax
+% <../close_species_list_html.m *close_species_list_html*> (fid_Spec) 
+
+% Description
+% Run this after print_species_list_html.m
+%
+% Input:
+%
+% * fidSpec : scalar
+
+fprintf(fidSpec, '      </table>\n\n');
+fprintf(fidSpec, '    </div> <!-- end of content -->\n\n');
+		
+fprintf(fidSpec, '    <div id="footer">\n');
+fprintf(fidSpec, '      <div class="aligncenter" style="padding:20px">\n');
+fprintf(fidSpec, '        &#169; 2016 Add-my-pet\n');
+fprintf(fidSpec, '      </div>\n');
+fprintf(fidSpec, '    </div>\n\n');
+fprintf(fidSpec, '  </div> <!-- main wrapper -->\n');
+fprintf(fidSpec, '</div> <!-- main -->\n');
+
+fprintf(fidSpec, '</BODY>\n');
+fprintf(fidSpec, '</HTML>\n');
+fclose(fidSpec);
+
+end
