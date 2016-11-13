d58557c1cf8baa8e7a28344784b805efa66a4f1c
baskooijman <bas.kooijman@vu.nl>
2016-11-13 17:38:17 +0100

    update

--- prt_about.m
+++ prt_about.m
@@ -143,7 +143,6 @@ fprintf(fid_about, '    <div id="content">\n');
 fprintf(fid_about, '    <H1 id = "portaltop">About Add-my-pet</H1>\n'); 
 fprintf(fid_about, '    <H2 id = "aims">Overview of the collection</H2>\n\n');
 
-
 fprintf(fid_about, '    <div class="sidelement2">\n');
 fprintf(fid_about, '      <img src="img/about/pie_Animalia.png" width="350px">\n');
 fprintf(fid_about, '      <div class = "caption">   \n');
