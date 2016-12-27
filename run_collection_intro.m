ae6d9bf36b78f2af48bd96154cc07f7a2be8cc3e
baskooijman <bas.kooijman@vu.nl>
2016-12-27 11:59:46 +0100

    script for updating collection

---
+++ run_collection_intro.m
@@ -0,0 +1,23 @@
+%% run_collection_intro
+% functions that runs collection overhead
+
+% make sure that debtool_M/taxa/ is updated
+% make sure that ../img/tree is updated
+% write_allStat;
+
+prt_species_list
+prt_species_tree_taxa_js
+prt_authors
+prt_pars
+prt_about
+
+%% cp to web
+
+% ../species_list.html 
+% ../pars.html
+% ../about.html
+
+% ../sys/species_list_*.js
+% ../img/pars/*.png
+% ../img/about/*.png 
+% ../img/tree/*.jpg and *.jpg.txt
