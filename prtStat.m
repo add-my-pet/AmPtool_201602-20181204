35a5a73379bd6c9227d54dd6260aceca0a02bf42
baskooijman <bas.kooijman@vu.nl>
2017-08-20 20:32:47 +0200

    print statistics

---
+++ prtStat.m
@@ -0,0 +1,24 @@
+%% prtStat
+% Print parameter or stastistics of selected entries names and values 
+
+%%
+function prtStat(taxa, var)
+  % created by Bas Kooijman 2017/08/20
+  
+  %% Syntax 
+  % <../prtStat.m *prtStat*>(taxa, var)
+  
+  %% Description
+  % Print parameter or statistic values for selected entries to screen. 
+  %
+  % Input:
+  %
+  % * taxa: cell-string with taxa
+  % * var: character string with prameter ot statistic
+  
+  %% Example of use 
+  % prtStat({'Paridae','Cyprinidae'}, 'p_M')
+  
+  [sel nm] = select_01(taxa); 
+  [val entries units label] = read_allStat(var); 
+  printpar(nm(sel), val(sel), [], [label{1}, ': ', units{1}])
