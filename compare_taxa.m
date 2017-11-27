06512fea2a1d525b23c0f3ce74fb44bedfa4966b
baskooijman <bas.kooijman@vu.nl>
2017-11-27 14:41:09 +0100

    optimization of code

--- compare_taxa.m
+++ compare_taxa.m
@@ -1,4 +1,4 @@
-%% compare taxa
+%% compare_taxa
 % makes graphs of survivor functions and a table of selected quantities for selected taxa
 
 %%
@@ -64,205 +64,136 @@ function tab = compare_taxa(llegend, nr, format)
   for i = 1:n_nr
   switch nr(i)
       
-  case 1 % T_typical (typical body temperature)
-  fig(i); txt_var = 'T_{typical}'; unit = 'C';
-  shstat_options('x_transform', 'none');
-  T_typical = K2C(read_allStat({'T_typical'}));
-  [Hfig, Hleg, val] = shstat(T_typical, llegend,[], i);  close(Hleg);
-  fig(Hfig); xlabel([txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
+    case 1 % T_typical (typical body temperature)
+      fig(i); txt_var = 'T_{typical}'; unit = 'C';
+      shstat_options('x_transform', 'none');
+      T_typical = K2C(read_allStat({'T_typical'}));
+      [Hfig, Hleg, val] = shstat(T_typical, llegend,[], i);  close(Hleg);
+      fig(Hfig); xlabel([txt_var, ', ', unit])
 
-  case 2 % kappa (allocation fraction to soma)
-  fig(i); txt_var = 'kap'; unit = '-';
-  shstat_options('x_transform', 'none');
-  [Hfig, Hleg, val] = shstat({'kap'}, llegend,[], i); close(Hleg);
-  fig(Hfig); xlabel([txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
+    case 2 % kappa (allocation fraction to soma)
+      fig(i); txt_var = 'kap'; unit = '-';
+      shstat_options('x_transform', 'none');
+      [Hfig, Hleg, val] = shstat({'kap'}, llegend,[], i); close(Hleg);
+      fig(Hfig); xlabel([txt_var, ', ', unit])
  
-  case 3 % [p_M] (spec somatic maintenance)
-  fig(i); txt_var = '[p_M]'; unit = 'J/d.cm^3';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'p_M'}, llegend, 'At T_{ref}', i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+    case 3 % [p_M] (spec somatic maintenance)
+      fig(i); txt_var = '[p_M]'; unit = 'J/d.cm^3';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'p_M'}, llegend, 'At T_{ref}', i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 4 % v (energy conductance)
+      fig(i); txt_var = 'v'; unit = 'cm/d';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('v', 's_M'); v = prod(vars,2);
+      [Hfig, Hleg, val] = shstat(v, llegend, 'At T_{ref}, post metam', i); close(Hleg); 
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 5 % [E_m] (reserve capacity)
+      fig(i); txt_var = '[E_m]'; unit = 'J/cm^3';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'E_m'}, llegend, [], i);  close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 6 % E_0 (initial energy)
+      fig(i); txt_var = 'E_0'; unit = 'J';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'E_0'}, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 7 % Ww_i (max wet weight)
+      fig(i); txt_var = 'Ww_i'; unit = 'g';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'Ww_i'}, llegend,[], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 8 % r: dWm/W_dWm (max growth over weight-at-that-moment)
+      fig(i); txt_var = 'max-r'; unit = '1/d';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('dWm', 'W_dWm', 'c_T'); r = vars(:,1)./vars(:,2)./vars(:,3);
+      [Hfig, Hleg, val] = shstat(r, llegend, 'At T_{ref}', i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 9 % R_i/Ww_i (max reprod rate over max weight)
+      fig(i); txt_var = 'spec-R_i'; unit = '#/d.g';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('R_i', 'c_T', 'Ww_i'); R_i = vars(:,1)./vars(:,2)./ vars(:,3);
+      [Hfig, Hleg, val] = shstat(R_i, llegend, 'At T_{ref}', i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 10 % p_Ri/Ww_i (max reprod power over max weight)
+      fig(i); txt_var = 'spec-p_{Ri}'; unit = 'J/d.g';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('p_Ri', 'c_T', 'Ww_i'); p_Ri = vars(:,1)./vars(:,2)./vars(:,3);
+      [Hfig, Hleg, val] = shstat(p_Ri, llegend, 'At T_{ref}', i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 11 % g (energy investment ratio)
+      fig(i); txt_var = 'g'; unit = '-';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'g'}, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 12 % s_M (acceleration)
+      fig(i); txt_var = 's_M'; unit = '-';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'s_M'}, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 13 % s_s (suply stress) 
+      fig(i); txt_var = 's_s'; unit = '-';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'s_s'}, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 14 % s_H^bp (maturity ratio)
+      fig(i); txt_var = 's_H^{bp}'; unit = '-';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'s_Hbp'}, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 15 % s_HL^bp (maturity density ratio)
+      fig(i); txt_var = 's_{HL}^{bp}'; unit = '-';
+      shstat_options('x_transform', 'none');
+      [Hfig, Hleg, val] = shstat({'s_HLbp'}, llegend, [], i); close(Hleg); 
+      fig(Hfig); xlabel([txt_var, ', ', unit])
+
+    case 16 % h_a (aging acceleration)
+      fig(i); txt_var = 'h_a'; unit = '1/d^2';
+      shstat_options('x_transform', 'log10');
+      [Hfig, Hleg, val] = shstat({'h_a'}, llegend, 'At T_{ref}', i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 17 % a_m (longevity)
+      fig(i); txt_var = 'a_m'; unit = 'd';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('a_m', 'c_T'); a_m = vars(:,1) .* vars(:,2);
+      [Hfig, Hleg, val] = shstat(a_m, llegend, 'At T_{ref}', i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 18 % p_Mi/p_Ai (som maint over assimilation)
+      fig(i); txt_var = 's_{MA}'; unit = '-';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('p_Si', 'p_Ai'); s_SA = vars(:,1)./vars(:,2);
+      [Hfig, Hleg, val] = shstat(s_SA, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+    case 19 % p_Ji/p_Ai (mat maint over assimilation)
+      fig(i); txt_var = 's_{JA}'; unit = '-';
+      shstat_options('x_transform', 'log10');
+      vars = read_allStat('p_Ji', 'p_Ai'); s_JA = vars(:,1)./vars(:,2);
+      [Hfig, Hleg, val] = shstat(s_JA, llegend, [], i); close(Hleg);
+      fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
+
+  end
+  
+  % save figure, add to table
   if ~isempty(format)
     saveas(gca,[txt_var, '.', format])
   end
   tab = add2tab(tab, i, txt_var, unit, val, sel);
 
-  case 4 % v (energy conductance)
-  fig(i); txt_var = 'v'; unit = 'cm/d';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('v', 's_M'); v = prod(vars,2);
-  [Hfig, Hleg, val] = shstat(v, llegend, 'At T_{ref}, post metam', i); close(Hleg); 
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 5 % [E_m] (reserve capacity)
-  fig(i); txt_var = '[E_m]'; unit = 'J/cm^3';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'E_m'}, llegend, [], i);  close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 6 % E_0 (initial energy)
-  fig(i); txt_var = 'E_0'; unit = 'J';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'E_0'}, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 7 % Ww_i (max wet weight)
-  fig(i); txt_var = 'Ww_i'; unit = 'g';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'Ww_i'}, llegend,[], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 8 % r: dWm/W_dWm (max growth over weight-at-that-moment)
-  fig(i); txt_var = 'max-r'; unit = '1/d';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('dWm', 'W_dWm', 'c_T'); r = vars(:,1)./vars(:,2)./vars(:,3);
-  [Hfig, Hleg, val] = shstat(r, llegend, 'At T_{ref}', i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 9 % R_i/Ww_i (max reprod rate over max weight)
-  fig(i); txt_var = 'spec-R_i'; unit = '#/d.g';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('R_i', 'c_T', 'Ww_i'); R_i = vars(:,1)./vars(:,2)./ vars(:,3);
-  [Hfig, Hleg, val] = shstat(R_i, llegend, 'At T_{ref}', i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 10 % p_Ri/Ww_i (max reprod power over max weight)
-  fig(i); txt_var = 'spec-p_{Ri}'; unit = 'J/d.g';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('p_Ri', 'c_T', 'Ww_i'); p_Ri = vars(:,1)./vars(:,2)./vars(:,3);
-  [Hfig, Hleg, val] = shstat(p_Ri, llegend, 'At T_{ref}', i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 11 % g (energy investment ratio)
-  fig(i); txt_var = 'g'; unit = '-';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'g'}, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 12 % s_M (acceleration)
-  fig(i); txt_var = 's_M'; unit = '-';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'s_M'}, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 13 % s_s (suply stress) 
-  fig(i); txt_var = 's_s'; unit = '-';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'s_s'}, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 14 % s_H^bp (maturity ratio)
-  fig(i); txt_var = 's_H^{bp}'; unit = '-';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'s_Hbp'}, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 15 % s_HL^bp (maturity density ratio)
-  fig(i); txt_var = 's_{HL}^{bp}'; unit = '-';
-  shstat_options('x_transform', 'none');
-  [Hfig, Hleg, val] = shstat({'s_HLbp'}, llegend, [], i); close(Hleg); 
-  fig(Hfig); xlabel([txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 16 % h_a (aging acceleration)
-  fig(i); txt_var = 'h_a'; unit = '1/d^2';
-  shstat_options('x_transform', 'log10');
-  [Hfig, Hleg, val] = shstat({'h_a'}, llegend, 'At T_{ref}', i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 17 % a_m (longevity)
-  fig(i); txt_var = 'a_m'; unit = 'd';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('a_m', 'c_T'); a_m = vars(:,1) .* vars(:,2);
-  [Hfig, Hleg, val] = shstat(a_m, llegend, 'At T_{ref}', i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 18 % p_Mi/p_Ai (som maint over assimilation)
-  fig(i); txt_var = 's_{MA}'; unit = '-';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('p_Si', 'p_Ai'); s_SA = vars(:,1)./vars(:,2);
-  [Hfig, Hleg, val] = shstat(s_SA, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  case 19 % p_Ji/p_Ai (mat maint over assimilation)
-  fig(i); txt_var = 's_{JA}'; unit = '-';
-  shstat_options('x_transform', 'log10');
-  vars = read_allStat('p_Ji', 'p_Ai'); s_JA = vars(:,1)./vars(:,2);
-  [Hfig, Hleg, val] = shstat(s_JA, llegend, [], i); close(Hleg);
-  fig(Hfig); xlabel(['_{10}log ', txt_var, ', ', unit])
-  if ~isempty(format)
-    saveas(gca,[txt_var, '.', format])
-  end
-  tab = add2tab(tab, i, txt_var, unit, val, sel);
-
-  end
   end
 
   shllegend(llegend);
