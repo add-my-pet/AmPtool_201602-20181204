216dbbff5121a1ff67b8a07d3c6d9ae30f23051f
Starrlight Augustine <starraugustine@users.noreply.github.com>
2018-08-20 10:03:48 +0200

    curator_report
    made it modular such that each step can be run independantly

--- curation/curator_report.m
+++ curation/curator_report.m
@@ -2,7 +2,7 @@
 % Produces a report to help curators analyse a submission
 
 %%
-function curator_report(speciesnm)
+function curator_report(speciesnm, stepnb)
   % created 2015/08/01 by Goncalo Marques
   % modified 2015/08/06 Dina Lika, 2018/01/23 by Bas Kooijman, 2018/04/15
   % Starrlight, 2018/08/20 Starrlight
@@ -26,6 +26,8 @@ function curator_report(speciesnm)
   % Input
   %
   % * speciesnm: string with species name
+  % * stepnb: scalar with the step number the report should start
+  % performing
   %  
   % Output is printed to screen
 
@@ -33,57 +35,70 @@ function curator_report(speciesnm)
   % This function is supposed to be run in the directory of the source
   % files: mydata_speciesnm, pars_init_speciesnm, 
   
-  %% Example of use
-  % curator_report('my_pet') 
+  %% Examples of use
+  %
+  % * curator_report('my_pet') 
+  % * curator_report('my_pet', 2) : and then only start the report at step
+  % two
 
+if  ~exist('stepnb', 'var')
+    stepnb = [];
+end
+  
 %%% Step 1: Check species name and lineage
-
 pointNumber = 1; 
 
-fprintf('\n%d. Checking if the species already in AmP:\n\n', pointNumber);
-
+if isempty(stepnb) || stepnb == 1 
+fprintf('\n Step %d. Checking if the species is already in AmP:\n\n', pointNumber);
 check_speciesnm(speciesnm);
+end 
 
-fprintf('Hit a key to continue\n'); pause
+if isempty(stepnb)
+    fprintf('Hit a key to continue\n'); pause
+end
 
 %%% Step 2: run check_data for data_0, data_1 and COMPLETE
-
 pointNumber = pointNumber + 1; 
 
-fprintf('\n%d. Data types:\n\n', pointNumber);
-
+if   isempty(stepnb) || stepnb == 2
+fprintf('\n Step %d. Data types:\n\n', pointNumber);
 check_data(speciesnm);
-
 fprintf('\nCheck the consistency between metaData and data.\n');
 fprintf('Check that the labels for each data type are used and consistent with the contents.\n');
-fprintf('Contact the web administrator with any new labels that should be added to the table (http://www.debtheory.org/wiki/index.php?title=Add-my-pet_Introduction)\n');
+fprintf('Contact the web administrator with any new labels that should be added to the table. \n');
+fprintf('http://www.debtheory.org/wiki/index.php?title=Add-my-pet_Introduction)\n');
+end
 
-fprintf('Hit a key to continue\n'); pause
+if isempty(stepnb)
+    fprintf('Hit a key to continue\n'); pause
+end
 
 %%% Step 3: check bibliography and url's
-
 pointNumber = pointNumber + 1; 
-fprintf([...
-    '\n%d. Check Bibliography: \n - verify that all the Urls work  \n', ...
-      '- make sure each reference has a DOI \n',...
-    '- species names need \emph{} \n' ...
-    '\n', pointNumber]);
 
+if   isempty(stepnb) || stepnb == 3
+    
+fprintf( '\n Step %d. Check Bibliography', pointNumber);
 [data, auxData, metaData] = feval(['mydata_', speciesnm]); % get metaData.biblist
 prt_my_pet_bib(speciesnm, metaData.biblist) % biblist2bib
 bib2bbl([speciesnm,'_bib']);                % runs Bibtex to convert bib to bbl
 bbl2html([speciesnm,'_bib']);               % converts bbl to html
 web([speciesnm,'_bib.html'],'-browser');    % open html of bibliography in system browser
 
-fprintf('Hit a key to continue\n'); pause
-delete([speciesnm,'_bib.bib'],[speciesnm,'_bib.html']); % delete produced files
+end
+
+if isempty(stepnb)
+    fprintf('Hit a key to continue\n'); pause
+    delete([speciesnm,'_bib.bib'],[speciesnm,'_bib.html']); % delete produced files
+end
+
 
 %%% Step 4: check extra parameters
-
 pointNumber = pointNumber + 1; 
 
-fprintf('\n%d. Checking extra parameters:\n\n', pointNumber);
-
+if   isempty(stepnb) || stepnb == 4
+    
+fprintf('\n Step %d. Checking extra parameters:\n\n', pointNumber);
 [data, auxData, metaData, txtData, weights] = feval(['mydata_', speciesnm]);
 [par, metaPar, txtPar] = feval(['pars_init_', speciesnm], metaData);
 standChem = addchem([], [], [], [], metaData.phylum, metaData.class);
@@ -104,13 +119,18 @@ end
 fprintf('\nCheck if these are all used in predict.\n');
 fprintf('Check if there should exist customized filters involving these parameters. \n Check that there is a discussion point relating to the use of these parameters. \n');
 
-fprintf('Hit a key to continue\n'); pause
+end
+
+if isempty(stepnb)
+    fprintf('Hit a key to continue\n'); pause
+end
+
 
 %%% Step 5: check which parameters were estimated:
-
 pointNumber = pointNumber + 1; 
 
-fprintf('\n%d. Checking choice of free parameters:\n\n', pointNumber);
+if   isempty(stepnb) || stepnb == 5
+fprintf('\n Step %d. Checking choice of free parameters:\n\n', pointNumber);
 
 freeFixedFields = fields(par.free);
 
@@ -133,14 +153,17 @@ end
 
 fprintf('\nCheck if the values above are reasonable and if there is enough data to estimate them.\n\n');
 
+end
 
-fprintf('Hit a key to continue\n'); pause; 
+if isempty(stepnb)
+    fprintf('Hit a key to continue\n'); pause
+end
 
 %%% Step 6: check implied properties 
-
 pointNumber = pointNumber + 1; 
 
-fprintf('\n%d. Check implied model properties and parameter values of my_pet. Creates my_pet.html.\n\n', pointNumber);
+if   isempty(stepnb) || stepnb == 6
+fprintf('\n Step %d. Check implied model properties and parameter values of my_pet. Creates my_pet.html.\n\n', pointNumber);
 prnt = input('Enter: 1 to compute statistics else 0 to continue: ');
 
 if prnt
@@ -152,17 +175,17 @@ if prnt
   
 end
 
-fprintf('Hit a key to continue\n'); pause; 
+end
 
-delete(['report_',speciesnm,'.html']); % delete produced file
-
-
-
-pause; 
+if isempty(stepnb)
+    fprintf('Hit a key to continue\n'); pause
+   delete(['report_',speciesnm,'.html']); % delete produced file
+end
 
 %%% Step 7: make sure figures are saved
 
 pointNumber = pointNumber + 1;
 
-fprintf('\n%d. Please after the curation process execute the run file with estim_option, results_output=2 \n\n', pointNumber);
-
+if isempty(stepnb)
+fprintf('\n Step %d. Please after the curation process execute the run file with estim_option, results_output=2 \n\n', pointNumber);
+end
